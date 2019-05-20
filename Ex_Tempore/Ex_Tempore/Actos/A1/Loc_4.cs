b321c3c7171de888582c9b7a66067f79197c4a26
RedBrickBurrito <a01568010@gmail.com>
2019-05-20 00:40:53 -0600

    update2

---
+++ Ex_Tempore/Ex_Tempore/Actos/A1/Loc_4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Tempore.Actos.A1
+{
+    class Loc_4
+    {
+    }
+}
---
+++ Ex_Tempore/Ex_Tempore/Actos/A1/Loc_5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Tempore.Actos.A1
+{
+    class Loc_5
+    {
+        public Loc_5()
+        {
+            //cuarto blanco
+        }
+    }
+}
